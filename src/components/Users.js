2acb10271ca0fc583d83a925de789b6c970b4816
Grinkov <44489570+vladislavGrinkov@users.noreply.github.com>
2019-05-27 01:34:56 +0300

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,10 +1,72 @@
 import React, {Component} from 'react'
 import User from './User';
 import InstaService from "../services/instaservice";
+import ErrorMessage from './ErrorMessage';
 
 
-export default function Users(){
-    
+export default class Users extends Component{
+   InstaService = new InstaService();
+
+   state = {
+        users: [],
+        error: false
+   }
+
+   componentDidMount(){
+       this.updateUsers();
+   }
+
+   updateUsers(){
+       this.InstaService.getAllUsers()
+       .then(this.onShowUsers)
+       .cath(this.onError)      
+   }
+   
+   onShowUsers = (users) => {
+       this.setState({
+           users,
+           error: false
+       })
+   }
+
+   onError = () => {
+       this.setState({
+           error: true
+       })
+   }
+
+   renderItems(arr){
+       return arr.map(item => {
+           return (
+               <div key = {item.id}>
+                <User 
+                    src = {item.src}
+                    alt = {item.altname}
+                    name = {item.name}
+                    min
+                />
+               </div>
+           )
+       })
+   }
+   
+   render(){
+       const {error, users} = this.state;
+       if(error){
+           return <ErrorMessage/>
+       }
+       const items = this.renderItems(users);
+       return(
+           <div className = "right">
+                 {items}
+                <div className = "users__block">
+                    {items}
+                </div>
+           </div>
+       )
+   }
+   
+    /*
     return(
         <div className = "right">
             <User
@@ -23,4 +85,5 @@ export default function Users(){
             </div>
         </div>
     )
+    */
 }
